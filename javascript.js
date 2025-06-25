bcfb248031190fceea9a18f8a8243e119a7c52c7
Sham Safarany <125689014+Shamsafarany@users.noreply.github.com>
2025-06-25 15:40:34 +0300

    fix overflow

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
-const screen = document.querySelector(".screen");
+const screen = document.querySelector("p");
 const numbers = document.querySelectorAll(".number");
 const signs = document.querySelectorAll(".sign");
 const equal = document.querySelector("#equal");
-
+const clear = document.querySelector("#clear");
 let start = false;
 function add (op1, op2) {
     return op1 + op2;
@@ -18,9 +18,10 @@ function multiply(op1, op2) {
 
 function divide (op1, op2) {
     if (op2 === 0) {
-        console.log("Error: / by 0!");
+       console.log("divide by 0");
+       return null;
     } else {
-        return op1 / op2;
+        return Math.round((op1 / op2 * 100))/ 100;
     }
 }
 
@@ -45,7 +46,10 @@ for (let i = 0; i < numbers.length; i++) {
             screen.innerHTML = "";
             start = true;
         }
-        screen.textContent += `${numbers[i].textContent}`;
+        if(screen.textContent.length < 10) {
+            screen.textContent += `${numbers[i].textContent}`;
+        }
+        
     })
 }
 for (let i = 0; i < signs.length; i++){
@@ -55,12 +59,41 @@ for (let i = 0; i < signs.length; i++){
     })
 }
 
+
 equal.addEventListener("click", () => {
+    let content = screen.innerHTML;
+    console.log(content);
     let match= screen.innerHTML.match(/[+\-x/]/);
     op = match[0];
-    op1 = Number(screen.innerHTML.substring(0, screen.innerHTML.search(`\\${op}`)));
-    op2 = Number((screen.innerHTML.substring(screen.innerHTML.search(`\\${op}`) + 1)));
-    screen.innerHTML = `=${operate(op1, op2, op)}`;
-    start = false;
+    if (content.startsWith("=")) {
+        content = content.slice(1);
+        console.log(content);
+    } 
+    op1 = Number(content.substring(0, content.search(`\\${op}`)));
+    op2 = Number((content.substring(content.search(`\\${op}`) + 1)));
+    if (op === null || op1 === null || op2 === null) {
+        console.log("missing");
+        return;
+    }
+    console.log(op);
+    console.log(op1);
+    console.log(op2);
+    
+    let currentRes = operate(op1, op2, op);
+    console.log(currentRes);
+    currentRes = Math.round((currentRes* 100))/ 100;
+    if (currentRes === null) {
+        screen.textContent= "Error!";
+    } else {
+        screen.innerHTML = `=${currentRes}`;
+    }
+    op1 = Number(currentRes);
+    start = true;
 });
+
+clear.addEventListener("click", ()=>{
+    screen.innerHTML = "0";
+    start = false;
+})
+
 
